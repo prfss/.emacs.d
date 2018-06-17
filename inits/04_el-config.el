1539644c28d22f17695267d24fce73fe4b08d369
s.sawada <gprfss4815@gmail.com>
2018-06-17 17:34:13 +0900

    Add merlin settings

--- inits/04_el-config.el
+++ inits/04_el-config.el
@@ -54,8 +54,14 @@
               auto-mode-alist))
 
 (setq opam-share (substring (shell-command-to-string "opam config var share") 0 -1))
-(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+(push (concat opam-share "/emacs/site-lisp") load-path)
 
 ;;; ocp-indent and merlin are installed through opam
 ;; ocp-indent
 (require 'ocp-indent)
+;; merling
+(autoload 'merlin-mode "merlin" "Merlin mode" t)
+(add-hook 'tuareg-mode-hook 'merlin-mode)
+(add-hook 'caml-mode-hook 'merlin-mode)
+(setq merlin-command 'opam)
+(setq merlin-ac-setup 'easy)
