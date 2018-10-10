6b5aadd3cc38997b2cd15164fe95ef55e617812c
prfss <gprfss4815@gmail.com>
2018-10-11 00:32:11 +0900

    Enable linum-mode

--- inits/02_window.el
+++ inits/02_window.el
@@ -5,9 +5,13 @@
 (setq-default tab-width 4 indent-tabs-mode nil)
 (recentf-mode 1)
 (show-paren-mode 1)
-(line-number-mode 1)
+                                        ;(line-number-mode 1)
 (column-number-mode 1)
 (tool-bar-mode 0)
 
 (if window-system
     (set-frame-parameter nil 'alpha 85))
+
+(if (version<= "26.0.50" emacs-version)
+    (global-display-line-numbers-mode)
+  (global-linum-mode))
