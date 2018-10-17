0ebd128a6d456b8948210f0a57e2c0a741078e58
prfss <gprfss4815@gmail.com>
2018-10-17 15:28:55 +0900

    Change YASnippet key bindings
    C-c C-e -> yas-expand

--- inits/04_el-config.el
+++ inits/04_el-config.el
@@ -25,7 +25,7 @@
   (yas-global-mode 1)
   (define-key yas-minor-mode-map (kbd "<tab>") nil)
   (define-key yas-minor-mode-map (kbd "TAB") nil)
-  (define-key yas-minor-mode-map (kbd "C-e") 'yas-expand)
+  (define-key yas-minor-mode-map (kbd "C-c C-e") 'yas-expand)
   )
 
 ;;; whitespace-mode
