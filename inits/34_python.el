ae144c875f74b68c59c99c8e186cfe07f1c2d073
prfss <gprfss4815@gmail.com>
2019-09-13 19:09:45 +0900

    Switch to python-mypy checker

--- inits/34_python.el
+++ inits/34_python.el
@@ -4,3 +4,7 @@
   :config
   (add-to-list 'company-backends 'company-jedi)
   (setq jedi:complete-on-dot t))
+
+;; flycheck
+(add-hook 'python-mode-hook
+          (lambda () (flycheck-select-checker 'python-mypy)))
