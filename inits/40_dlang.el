51ff4f7a53b7f672faf40f0ce2c9a17a88f628a3
prfss <gprfss4815@gmail.com>
2019-10-04 07:25:26 +0900

    D language

---
+++ inits/40_dlang.el
@@ -0,0 +1,13 @@
+(use-package d-mode
+  :config
+  (add-hook 'd-mode-hook #'lsp)
+  (add-hook 'd-mode-hook (lambda () (setq c-basic-offset 4)))
+  (add-hook 'd-mode-hook
+            (lambda () (add-hook 'before-save-hook #'lsp-format-buffer)))
+  (add-to-list 'lsp-language-id-configuration '(d-mode . "d"))
+  (lsp-register-client
+   (make-lsp-client
+    :new-connection (lsp-stdio-connection '("~/.dub/packages/.bin/dls-latest/dls"))
+    :major-modes '(d-mode)
+    :server-id 'dls
+    )))
