1f6e2f088b0254a5f387e83bdf0311ca7dfdf896
prfss <gprfss4815@gmail.com>
2019-10-25 23:24:16 +0900

    Change whitespace-mode settings

--- inits/13_whitespace.el
+++ inits/13_whitespace.el
@@ -2,18 +2,20 @@
   :diminish global-whitespace-mode
   :config
   (setq whitespace-action '(auto-cleanup))
-  (setq whitespace-style '(face trailing tabs empty))
+  (setq whitespace-style '(face empty indentation tabs tab-mark trailing))
   (let ((my/bg-color (face-background 'default)))
+    (set-face-attribute 'whitespace-empty nil
+                        :foreground "DeepPink"
+                        :background my/bg-color
+                        :underline t)
+    (set-face-attribute 'whitespace-indentation nil
+                        :foreground "Cyan"
+                        :background my/bg-color
+                        :underline t)
     (set-face-attribute 'whitespace-trailing nil
                         :foreground "DeepPink"
                         :background my/bg-color
                         :underline t)
     (set-face-attribute 'whitespace-tab nil
-                        :foreground "Cyan"
-                        :background my/bg-color
-                        :underline t)
-    (set-face-attribute 'whitespace-empty nil
-                        :foreground "DeepPink"
-                        :background my/bg-color
-                        :underline t))
+                        :inherit whitespace-indentation))
   (global-whitespace-mode 1))
