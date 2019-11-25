18777018862fbc89a0b93c40dba09f1f6ead50ae
prfss <gprfss4815@gmail.com>
2019-11-25 19:19:58 +0900

    Dlang: disable indentation on save

--- inits/40_dlang.el
+++ inits/40_dlang.el
@@ -2,7 +2,6 @@
   :config
   (add-hook 'd-mode-hook #'lsp)
   (add-hook 'd-mode-hook (lambda () (setq c-basic-offset 4)))
-  (add-hook 'd-mode-hook #'enable-indent-buffer-on-save)
   (add-to-list 'lsp-language-id-configuration '(d-mode . "d"))
   (lsp-register-client
    (make-lsp-client
