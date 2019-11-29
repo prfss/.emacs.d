71429cd7d89c8247d2fcf3548f995cb1e3a263f5
prfss <gprfss4815@gmail.com>
2019-11-29 21:15:41 +0900

    Refactoring: remove redundant code

--- inits/36_rust.el
+++ inits/36_rust.el
@@ -1,21 +1,5 @@
 (use-package rust-mode
   :config
-  ;;; prefer flymake in rust-mode
-  ;(add-hook 'rust-mode-hook (lambda () (flycheck-mode -1)))
-  (add-hook 'rust-mode-hook #'rust-enable-format-on-save)
-  )
+  (rust-enable-format-on-save))
 
 (use-package flycheck-rust)
-
-;; (use-package lsp-mode
-;;   :config
-;;   (lsp-register-client
-;;    (make-lsp-client
-;;     :new-connection (lsp-stdio-connection '("rls"))
-;;     :major-modes '(rust-mode)
-;;     :priority 0
-;;     :server-id 'rls+clippy
-;;     :initialized-fn (lambda (workspace)
-;;                       (with-lsp-workspace workspace (lsp--set-configuration `(:rust (:clippy_preference "on")))))
-;;     :notification-handlers (lsp-ht ("window/progress" 'lsp-clients--rust-window-progress))
-;;     )))
