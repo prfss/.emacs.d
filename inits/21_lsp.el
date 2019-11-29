a0f3629986741d5a78c78fec64b36cf824095c06
prfss <gprfss4815@gmail.com>
2019-11-29 21:13:56 +0900

    Use gopls

--- inits/21_lsp.el
+++ inits/21_lsp.el
@@ -1,10 +1,11 @@
 (use-package lsp-mode
-  :hook ((python-mode ruby-mode rust-mode) . lsp)
+  :hook ((go-mode python-mode ruby-mode rust-mode) . lsp)
   :commands lsp
   :config
   (setq lsp-rust-clippy-preference "on")
   (setq lsp-log-io t)
   (setq lsp-prefer-flymake nil)
+  (setq lsp-enable-file-watchers nil)
   (defun lsp-format-buffer-on-save ()
     (interactive)
     (add-hook 'before-save-hook #'lsp-format-buffer nil t))
@@ -13,4 +14,7 @@
 (use-package company-lsp
   :commands company-lsp)
 
-(use-package lsp-ui)
+(use-package lsp-ui
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+  )
--- inits/35_golang.el
+++ inits/35_golang.el
@@ -2,9 +2,3 @@
   :hook
   ((go-mode) .
    (lambda () (set (make-local-variable 'whitespace-style) '(face trailing empty)))))
-
-(use-package company-go
-  :config
-  (add-to-list 'company-backends 'company-go))
-
-(use-package go-eldoc)
